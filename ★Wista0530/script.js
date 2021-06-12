2490d82b28af9537a34b2ce4b3a77b38566bb8c6
IppeiKono <82949617+IppeiKono@users.noreply.github.com>
2021-06-12 23:17:51 +0900

    Delete ★Wista0530 directory

--- ★Wista0530/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-	$('.tab').click(function(){
-		$('.is-active').removeClass('is-active');
-		$(this).addClass('is-active');
-		$('.is-show').removeClass('is-show');
-		const index = $(this).index();
-		$('.panel').eq(index).addClass('is-show');
-	});
-});
